b42151b782db4b27ec19ce061bb9f5879c1c40f5
Beatriz Torres <btorre0@csu.fullerton.edu>
2024-11-25 14:43:41 -0800

    hold

--- allocator.c
+++ allocator.c
@@ -15,9 +15,25 @@ static mem_block_t *find_free_block(size_t size) {
     return NULL;
 }
 
-void* get_me_blocks(ssize_t how_much) {
-    void* ptr = sbrk(0);
-    sbrk(how_much);
+void *get_me_blocks(ssize_t how_much) {
+    pthread_mutex_lock(&allocator_mutex);
+
+    void *ptr = sbrk(0);
+
+    if (ptr == (void *)-1) {
+        perror("sbrk failed to get current break");
+        pthread_mutex_unlock(&allocator_mutex);
+        return NULL;
+    }
+
+    if (sbrk(how_much) == (void *)-1) {
+        perror("sbrk failed to increase break");
+        pthread_mutex_unlock(&allocator_mutex);
+        return NULL;
+    }
+
+    pthread_mutex_unlock(&allocator_mutex);
+    // sbrk(how_much);
     return ptr;
 }
 
@@ -33,20 +49,25 @@ static void split_block(mem_block_t *block, size_t size) {
 }
 
 void allocator_init(size_t size) {
-    pthread_mutex_lock(&allocator_mutex);
-    base = sbrk(0); // im getting a warning (because of my computerrr)
+    if (size < sizeof(mem_block_t)) {
+        fprintf(stderr, "Size is too small\n");
+        exit(1);
+    }
 
-    if (base == (void *)-1) {
+    pthread_mutex_lock(&allocator_mutex);
+    base = get_me_blocks(size);
+
+    if (base == NULL) {
         fprintf(stderr, "Failed to allocate memory\n");
         pthread_mutex_unlock(&allocator_mutex);
         exit(1);
     }
-    // free block
-    mem_block_t *block = (mem_block_t *)base;
-    block->size = size - sizeof(mem_block_t);
-    block->free = 1;
-    block->next = NULL;
-    free_list = block;
+
+    // mem_block_t *block = (mem_block_t *)base;
+    free_list = (mem_block_t *)base;
+    free_list->size = size - sizeof(mem_block_t);
+    free_list->free = 1;
+    free_list->next = NULL;
 
     pthread_mutex_unlock(&allocator_mutex);
 }
@@ -56,16 +77,22 @@ void *allocator_malloc(size_t size) {
         return NULL;
     }
 
+    size = (size + sizeof(void *) - 1) & ~(sizeof(void *) - 1);
+
     pthread_mutex_lock(&allocator_mutex);
+
     mem_block_t *block = find_free_block(size);
 
-    while (block) {
-        if (block->free && block->size >= size) {
-            block->free = 0;
-            return (void *)(block + 1);
+    if (block) {
+        if (block->size > size + sizeof(mem_block_t)) {
+            split_block(block, size);
         }
-        block = block->next;
+        block->free = 0;
+        pthread_mutex_unlock(&allocator_mutex);
+        memset((void *)(block + 1), 0, size);
+        return (void *)(block + 1);
     }
+
     pthread_mutex_unlock(&allocator_mutex);
     return NULL;
 }
@@ -74,16 +101,19 @@ void allocator_free(void *ptr) {
     if (ptr == NULL) {
         return;
     }
-    mem_block_t *block = (mem_block_t *)(char *)ptr - sizeof(mem_block_t);
+    pthread_mutex_lock(&allocator_mutex);
+    mem_block_t *block = (mem_block_t *)((char *)ptr - sizeof(mem_block_t));
     block->free = 1;
 
     mem_block_t *current = free_list;
-    while (current->next) {
-        if (current->free && current->next->free) {
+    while (current) {
+        if ((char *)current + sizeof(mem_block_t) + current->size ==
+            (char *)current->next) {
             current->size += current->next->size + sizeof(mem_block_t);
             current->next = current->next->next;
+        } else {
+            current = current->next;
         }
-        current = current->next;
     }
     pthread_mutex_unlock(&allocator_mutex);
 }
@@ -113,5 +143,5 @@ void allocator_cleanup() {
     pthread_mutex_destroy(&allocator_mutex);
     free_list = NULL;
     base = NULL;
-    pthread_mutex_unlock(&allocator_mutex);
+    // pthread_mutex_unlock(&allocator_mutex);
 }
--- testing.c
+++ testing.c
@@ -6,7 +6,7 @@
 void malloc_free_test() {
     allocator_init(1024);
     char *ptr = (char *)allocator_malloc(10);
-    strcpy(ptr, "Hello");
+    // strcpy(ptr, "Hello");
     if (ptr == NULL) {
         printf("Failed to allocate memory\n");
         return;
@@ -27,7 +27,7 @@ void realloc_test() {
     allocator_init(1024);
     char *ptr = (char *)allocator_malloc(10);
     strcpy(ptr, "Hello");
-    
+
     if (ptr == NULL) {
         printf("Failed to allocate memory\n");
         return;
@@ -44,12 +44,78 @@ void realloc_test() {
 
     strcpy(ptr, "Hello World");
     printf("reallocated and wrote - ptr: %s\n", ptr);
+    allocator_free(ptr);
 
     allocator_cleanup();
 }
 
-int main() {
-    malloc_free_test();
-    realloc_test();
+void merging_test() {
+    allocator_init(1024);
+    printf("Testing merging of free blocks...\n");
+
+    void* ptr1 = allocator_malloc(100);
+    void* ptr2 = allocator_malloc(100);
+
+    allocator_free(ptr1);
+    allocator_free(ptr2);
+
+    void* ptr3 = allocator_malloc(200);
+    if (ptr3) {
+        printf("Merge successful.\n");
+    } else {
+        printf("Merge failed.\n");
+    }
+
+    allocator_cleanup();
+}
+
+// #define NUM_THREADS 10
+
+// void* thread_worker(void *arg) {
+//     int *num = (int *)arg;
+//     printf("Thread %d\n", *num);
+//     free(num);
+//     return NULL;
+// }
+
+// void thread_test() {
+//     allocator_init(1024);
+//     pthread_t threads[NUM_THREADS];
+    
+//     for (int i = 0; i < NUM_THREADS; i++) {
+//         int *arg = malloc(sizeof(int));
+//         if (arg == NULL) {
+//             printf("Failed to allocate memory\n");
+//             exit(1);
+//         }
+//         *arg = i;
+//         pthread_create(&threads[i], NULL, thread_worker, arg);
+//     }
+
+//     for (int i = 0; i < NUM_THREADS; i++) {
+//         pthread_join(threads[i], NULL);
+//     }
+
+//     allocator_cleanup();
+// }
+
+int main(int argc, char* argv[]) {
+        if (argc > 1 && strcmp(argv[1], "-t") == 0) {
+            int num_tests = atoi(argv[2]);
+            if (num_tests == 1) {
+                malloc_free_test();
+            } else if (num_tests == 2) {
+                realloc_test();
+            } else if (num_tests == 3) {
+                merging_test();
+            } else {
+                printf("Invalid test number\n");
+            }
+        } else {    
+            malloc_free_test();
+            realloc_test();
+            merging_test();
+        }
+    // thread_test();
     return 0;
 }
